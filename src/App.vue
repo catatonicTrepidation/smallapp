057c4d9741ce802c1aa6544ab6d7db4c469c7b51
catatonicTrepidation <catatonictrepidation@gmail.com>
2018-12-15 11:37:33 +0900

    added a bit more vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <a-scene>
-        <a-sphere position="-2 1 1" src="../static/images/sky.png"></a-sphere>
-        <a-sphere position="1 1 1" v-bind:src = "imgurl"></a-sphere>
-        <a-sphere v-bind:position="custompos">{{desc}}</a-sphere>
+        <a-sphere v-on:click="clickTest" position="-2 1 1" src="../static/images/sky.png"></a-sphere>
+        <a-sphere v-bind:position="randpos" v-bind:src = "imgurl"></a-sphere>
+        <a-sphere v-on:click="getRandPos" v-bind:position="custompos">{{desc}}</a-sphere>
         <!-- Sky id="Sky" -->
         <a-sky ref="sky"
              id="image-360"
              radius="10"
              src="../static/images/sky.png"></a-sky>
         </a-sky>
+        <a-camera>
+            <a-cursor id="cursor"></a-cursor>
+        </a-camera>
     </a-scene>
 </template>
 
@@ -22,6 +25,31 @@ export default {
             imgurl: "../static/images/sky.png"
         }
     },
+    computed: {
+        randpos: function() {
+            return [1, this.getRandomInt(-2,6), 1].join(' ');
+        }
+    },
+
+    methods: {
+        clickTest: function (event) {
+            console.log("Clicked!");
+            if (event) {
+                console.log(event.target.tagName);
+            }
+        },
+        getRandPos: function (event) {
+            this.custompos = [this.getRandomInt(-2,2), this.getRandomInt(0,3), this.getRandomInt(-2,2)].join(' ');
+        },
+        getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+        }
+    }
+
+
+    
 }
 
 </script>
