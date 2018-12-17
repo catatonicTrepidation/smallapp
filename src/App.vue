b44e643bf9c839f62cab7d5fdccdd027916b8981
catatonicTrepidation <catatonictrepidation@gmail.com>
2018-12-17 22:58:24 +0900

    tested adding objects from fragment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <a-scene>
-        <a-sphere v-on:click="clickTest" position="-2 1 1" src="../static/images/ball2.png"></a-sphere>
+        <a-sphere v-on:click="clickTest" position="-3 1 1" src="../static/images/ball2.png"></a-sphere>
         <a-sphere v-bind:position="randpos" v-bind:src = "ballsrc"></a-sphere>
         <a-sphere color="#141edc" v-on:click="getRandPos" v-bind:position="custompos">{{desc}}</a-sphere>
 
@@ -17,6 +17,8 @@
         </a-camera>
     </a-scene>
 </template>
+
+
 
 <script>
 import tower from './components/tower.vue'
@@ -52,10 +54,30 @@ export default {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-        }
+        },
+
+
+        addFloatingOrbs() {
+            // trying out this way of adding objects
+        var frag = this.fragmentFromString(`
+            <a-entity position="-5 0 -5">
+                <a-sphere color="#1edc14" radius="0.5" position="-1 -1 -1"></a-sphere>
+                <a-sphere color="#dc141e" radius="0.5" position="1 -1 -1"></a-sphere>
+                <a-sphere color="#141edc" radius="0.5" position="0 -1 1"></a-sphere>
+                <a-sphere color="#115656" radius="0.5" position="0 1 0"></a-sphere>
+            </a-entity>
+        `);
+        document.getElementsByTagName('a-scene')[0].appendChild(frag);
+    },
+
+    fragmentFromString(strHTML) {
+            return document.createRange().createContextualFragment(strHTML);
+      }
+    },
+
+    mounted () {
+        this.addFloatingOrbs(); // this probably doesn't need to be called from here, but i kinda just wanted to test it, i guess
     }
-
-
     
 }
 
